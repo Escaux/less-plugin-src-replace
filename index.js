8ada3ee5e715aec479f19b157d5bf81b5fa48c95
Jorge Solis <jso@escaux.com>
2016-10-19 13:09:26 +0200

    Make it works as a lessc plugin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ LessSrcReplace.prototype = {
         pluginManager.addVisitor(new SrcVisitor(this.options));
     },
     setOptions: function(options) {
-        this.options = options || {};
+        this.options = JSON.parce(options) || {};
     }
 };
 
